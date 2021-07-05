465c1e1770136fdc0defa8d223e22a0c039c3396
LesGayard <leslie.123@hotmail.fr>
2021-07-05 09:43:30 +0200

    Tile class implementation maybe put it abstract

---
+++ src/com/leslie/realm/level/tile/Tile.java
@@ -0,0 +1,33 @@
+package com.leslie.realm.level.tile;
+
+import com.leslie.realm.graphics.Screen;
+import com.leslie.realm.graphics.Sprite;
+
+/* Each tile should have its own position, its own sprite*/
+
+/*Make it abstract ???*/
+public class Tile {
+
+    /*Find its own position*/
+    public int x;
+    public int y;
+
+    /*attach a sprite to it */
+    public Sprite sprite;
+
+    /*CONSTRUCTOR*/
+    public Tile(Sprite sprite){
+        this.sprite = sprite;
+    }
+
+    /* what information do i need to render the tile */
+    public void render (int x, int y, Screen screen){
+
+    }
+
+    public boolean solid(){
+        return false;
+    }
+
+
+}
